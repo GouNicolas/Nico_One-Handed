b3e9483486bd06ef152a20248d76f81c3ff35071
Nikoala4 <104569770+Nikoala4@users.noreply.github.com>
2024-04-26 16:46:48 +0200

    Vite faut save ça !

--- game/src/bin/model/Deck.java
+++ game/src/bin/model/Deck.java
@@ -5,8 +5,28 @@ public class Deck {
     ArrayList<Card> deck = new ArrayList();
     
     public Deck(ArrayList<Card> deck){
-        this.deck = new ArrayList();
+        this.deck = deck;
     }
 
-    
+    public Deck shuffle(Deck deck){
+        for (Suit suit : Suit.values){
+            for (Rank rank : Rank.values){
+                Card tempCard = new Card(rank,suit);
+                Deck.add(tempCard);
+            }
+        }
+        return Collections.shuffle(deck);
+    }
+
+    public void printDeck(Deck deck){
+        for (Card card : deck){
+            System.out.println(card);
+        }
+    }
+
+    public static void main(String[] args){
+        Deck deck = new Deck;
+        printDeck(deck);
+    }
+
 }
