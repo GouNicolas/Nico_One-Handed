cd0bca0ebd40b84a4ededec2050865e4873be56b
Nikoala4 <104569770+Nikoala4@users.noreply.github.com>
2024-04-26 16:00:05 +0200

    Implemented consctructor for the Deck

--- game/src/bin/model/Deck.java
+++ game/src/bin/model/Deck.java
@@ -4,4 +4,9 @@ import java.util.ArrayList;
 public class Deck {
     ArrayList<Card> deck = new ArrayList();
     
+    public Deck(ArrayList<Card> deck){
+        this.deck = new ArrayList();
+    }
+
+    
 }
